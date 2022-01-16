df9b4706df50edce645e59a66c40cdeee0e09fc0
Nele Braun <nelebraun@Neles-MacBook-Air-2.local>
2022-01-16 22:17:30 +0100

    remove unused var

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Form from "./Form";
